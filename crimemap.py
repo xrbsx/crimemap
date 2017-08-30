8004664b049a51ee2e398bf73d1e0eab4d662b9a
Rafael Barbosa <rafaelb.barbosa@gmail.com>
2017-08-30 01:35:49 -0300

    add new crimes funcionality

--- crimemap.py
+++ crimemap.py
@@ -3,17 +3,26 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import json
+
 app = Flask(__name__)
 DB = DBHelper()
 
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
+    return home()
+
 @app.route("/")
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print e
-        data = None
-    return render_template("home.html", data=data)
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes)
 
 @app.route("/add", methods=["POST"])
 def add():
