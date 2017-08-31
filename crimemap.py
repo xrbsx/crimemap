924c7b50fb55d23421603379c5394f3dd92053d5
Rafael Barbosa <rafaelb.barbosa@gmail.com>
2017-08-31 10:35:02 -0300

    First Release

--- crimemap.py
+++ crimemap.py
@@ -3,34 +3,45 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import datetime
+import dateparser
 import json
+import string
 
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 @app.route("/")
-def home():
+def home(error_message=None):
     #import pdb; pdb.set_trace()
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 @app.route("/add", methods=["POST"])
 def add():
     try:
         data = request.form.get("userinput")
         DB.add_input(data)
-        return render_template("home.html")
     except Exception as e:
         print e
     return home()
@@ -42,6 +53,17 @@ def clear():
     except Exception as e:
         print e
     return home()
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
